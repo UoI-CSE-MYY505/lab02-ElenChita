65b7359d8a03f1056430346220cabb491af4dfae
ElenaChita <xhtaelena@gmail.com>
2024-10-18 12:32:00 +0300

    Finish Lab2

--- lab02.s
+++ lab02.s
@@ -9,10 +9,16 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a1, 7    # unsigned
     li a2, 1
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+    add s0, zero, zero
+loop:
+    beq a1, 0 done # if a1 =0 done
+    lw t1, 0(a0) #take the address of the first element
+    bne a2, t1, nextStep # if a2 != t1 nextSteptarget
+    add s0, a0, zero #matching element's address in s0
+nextStep:
+    addi a0, a0, 4 #for next word
+    addi a1, a1, -1
+    j loop  # jump to loop
 done:
     addi a7, zero, 10 
     ecall
